18f73a1fccba206a27499c9a7b05abd92a45cd7e
hptme <hpttme@gmail.com>
2014-08-15 17:46:17 +0800

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Jinglin::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'jinlinchiayi',
+    password:             'g3bjo4dk4',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  
 end
